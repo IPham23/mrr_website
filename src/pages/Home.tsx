c7c554eb5854d80caab578d6ff4a812dcafb0b6e
IPham23 <iancoladilla23@gmail.com>
2025-09-30 10:58:37 +0800

    finished Homepage desktop view
    applied Tailwind framework in Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,31 +4,59 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-    <div className="home wrapper">
-      <section className="details">
+    <div className="max-w-[1600px] mx-auto overflow-x-hidden">
+      <section className="w-1/2 mx-[210px] pt-20">
         <img src="/src/assets/MANILA.png" alt="Manila" />
-        <p className="paragraph">
+        <p className="px-30 py-10 text-center">
           Welcome to Manila Runway Republic, the ultimate destination for young
           fashion enthusiasts! We are dedicated to nurturing the talents of
           aspiring models and creating high-quality events for kids and teens. Our
           platform offers a comprehensive range of opportunities, from
           professional photoshoots to dazzling fashion shows.
         </p>
-        <img className="runway" src="/src/assets/RUNWAY.png" alt="Runway" />
+        <img className="justify-self-end pr-16" src="/src/assets/RUNWAY.png" alt="Runway" />
 
       </section>
-      <section className="images">
-        <img src="/src/assets/HomeImg1.png" alt="Home Image 1" />
-        <img src="/src/assets/HomeImg2.png" alt="Home Image 2" />
-        <img src="/src/assets/HomeImg3.png" alt="Home Image 3" />
-        <img src="/src/assets/HomeImg4.png" alt="Home Image 4" />
+      <section className="w-100% px-30 flex items-end translate-y-[-45%]">
+        <img src="/src/assets/HomeImg1.png" 
+            alt="Home Image 1" 
+            className="transition-transform duration-400 ease-in-out hover:translate-x-[5px]"
+        />
+        <img src="/src/assets/HomeImg2.png" 
+            alt="Home Image 2" 
+            className="ml-[-70px] transition-transform duration-400 ease-in-out hover:translate-x-[5px]"
+        />
+        <img src="/src/assets/HomeImg3.png" 
+            alt="Home Image 3" 
+            className="ml-[-70px] transition-transform duration-400 ease-in-out hover:translate-x-[5px]"
+        />
+        <img src="/src/assets/HomeImg4.png" 
+            alt="Home Image 4" 
+            className="ml-[-145px] transition-transform duration-400 ease-in-out hover:translate-x-[5px]"
+        />
+
       </section>
-      <section className="brands">
-        <h2>Brand Partners</h2>
-        <div className="brandLogos">
-          <img src="/src/assets/logos/AlicioThailand.jpg" alt="Alicio Thailand logo" />
-          <img src="/src/assets/logos/Diesel.jpg" alt="Diesel logo" />
-          <img src="/src/assets/logos/DKNY.jpg" alt="DKNY logo" />
+      <section className="px-20 translate-y-[-80%]">
+        <h2 className="text-2xl font-bold mb-4">Brand Partners</h2>
+        <div className="flex w-[100%] gap-8 justify-around">
+          <div className="flex justify-center items-center">
+            <img src="/src/assets/logos/AlicioThailand.jpg" 
+                alt="Alicio Thailand logo" 
+                className="w-50"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <img src="/src/assets/logos/Diesel.jpg" 
+                alt="Diesel logo" 
+                className="w-50"
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            <img src="/src/assets/logos/DKNY.jpg" 
+                alt="DKNY logo" 
+                className="w-50"
+            />
+          </div>
         </div>
       </section>
     </div>
