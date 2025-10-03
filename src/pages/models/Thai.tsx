14fbb8d455f67a5e704916a38c88baea652c6b1c
angelo0908 <72687749+angelo0908@users.noreply.github.com>
2025-10-03 16:59:24 +0800

    Fix: Animation of Models Page

--- src/pages/models/Thai.tsx
+++ src/pages/models/Thai.tsx
@@ -1,11 +1,33 @@
+
+import "./ThaiGallery.css";
+
 const Thai = () => {
-    return (
-        <>
-        <section>
-            <h1>Thai</h1>
-        </section>
-        </>
-    );
+  return (
+    <div className="modelsWrapper">
+      {/* Main image */}
+      <img className="modelsImg" src="../src/assets/TITLE-THAI.png" alt="" />
+
+      {/* Models Grid */}
+      <div className="mrrModelsGrid">
+        {/* Row 1 */}
+        <div className="mrrRow">
+          <div className="mrrModelCard">
+            <img src="../src/assets/Artboard 10.png" alt="" />
+          </div>
+          <div className="mrrModelCard">
+            <img src="../src/assets/Artboard 11.png" alt="" />
+          </div>
+          <div className="mrrModelCard">
+            <img src="../src/assets/Artboard 12.png" alt="" />
+          </div>
+          <div className="mrrModelCard">
+            <img src="../src/assets/Artboard 13.png" alt="" />
+          </div>
+        </div>
+        {/* You can add more rows here later */}
+      </div>
+    </div>
+  );
 };
 
 export default Thai;
